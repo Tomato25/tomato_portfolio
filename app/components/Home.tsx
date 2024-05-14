b7a1945240836c530c8c3b3744c445734fb91867
Hrvoje <htomic4@gmail.com>
2024-05-14 17:32:50 +0200

    fix

---
+++ app/components/Home.tsx
@@ -0,0 +1,115 @@
+"use client";
+import { FaGithubSquare, FaLinkedin, FaTelegramPlane } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import ProfileImg from "@/public/Images/profileImg.jpg";
+import { iconContainerVariants } from "../animations/Animations";
+
+function Home() {
+  return (
+    <div className="min-h-screen z-10 flex flex-col md:mb-16 w-screen sm:max-w-6xl xl:max-w-screen-2xl px-8 mx-auto">
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-16 flex-1 md:mb-12 mb-8">
+        <motion.div initial={{x:-200, opacity:0}} animate={{x:1, opacity:1}} transition={{delay:0.5, duration:0.3}}  className="flex flex-col gap-6 items-center justify-center lg:w-1/2">
+          <p className="text-pink font-poppins md:text-2xl xl:text-3xl  text-2xl lg:text-left text-center leading-8">
+            Software developer and Computing graduate with freelance and
+            internship experience.
+          </p>
+          <p className="text-pink leading-7 font-poppins md:text-2xl xl:text-3xl  text-2xl lg:text-left text-center">
+            Passionate about producing clean and reusable code by utilizing
+            modern technologies.
+          </p>
+          <p className="text-pink leading-7 font-poppins md:text-2xl xl:text-3xl  text-2xl lg:text-left text-center">
+            Looking for the opportunity to further develop my skills and
+            offering web development services.
+          </p>
+        </motion.div>
+        <motion.div initial={{x:200, opacity:0}} animate={{x:1, opacity:1}} transition={{delay:0.5, duration:0.3}}  className="flex items-center lg:justify-end justify-center w-1/2 lg:h-auto">
+          <Image
+            className="rounded-full"
+            src={ProfileImg}
+            alt="avatar"
+            height={400}
+            width={400}
+          />
+        </motion.div>
+      </div>
+
+      <div className=" flex gap-6 flex-row justify-center items-center mb-16 text-4xl z-10">
+        <motion.div
+          variants={iconContainerVariants}
+          initial="hidden"
+          animate="visible"
+          className="flex flex-row h-100 justify-center items-center gap-8 md:gap-16 "
+        >
+          <motion.a
+            variants={iconContainerVariants}
+            whileHover={{
+              rotate: 360,
+              scale: 1.1,
+              backgroundColor: "#DDACE6",
+              transition: { duration: 0.8, type: "spring" },
+            }}
+            whileTap={{ rotate: -20, scale: 0.8 }}
+            className="md:h-16 md:w-16 h-12 w-12 md:text-4xl text-3xl  flex items-center justify-center rounded-full bg-lightGreen text-green cursor-pointer"
+            href="https://github.com/Tomato25/"
+            target="_blank"
+          >
+            <FaGithubSquare />
+          </motion.a>
+          <motion.a
+            variants={iconContainerVariants}
+            whileHover={{
+              rotate: 360,
+              scale: 1.1,
+              backgroundColor: "#DDACE6",
+
+              transition: { duration: 0.8, type: "spring" },
+            }}
+            whileTap={{ rotate: -20, scale: 0.8 }}
+            className="md:h-16 md:w-16 h-12 w-12 md:text-4xl text-3xl flex items-center justify-center rounded-full bg-lightGreen text-green  cursor-pointer"
+            href="https://www.linkedin.com/in/hrvoje-tomic/"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </motion.a>
+
+          <motion.a
+            variants={iconContainerVariants}
+            whileHover={{
+              rotate: 360,
+              scale: 1.1,
+              backgroundColor: "#DDACE6",
+
+              transition: { duration: 0.8, type: "spring" },
+            }}
+            whileTap={{ rotate: -20, scale: 0.8 }}
+            className="md:h-16 md:w-16 h-12 w-12 md:text-4xl text-3xl flex items-center justify-center rounded-full bg-lightGreen text-green cursor-pointer"
+            href="mailto:htomic4@gmail.com"
+            target="_blank"
+          >
+            <MdEmail />
+          </motion.a>
+          <motion.a
+            variants={iconContainerVariants}
+            whileHover={{
+              rotate: 360,
+              scale: 1.1,
+              backgroundColor: "#DDACE6",
+
+              transition: { duration: 0.8, type: "spring" },
+            }}
+            whileTap={{ rotate: -20, scale: 0.8 }}
+            className="md:h-16 md:w-16 h-12 w-12 md:text-4xl  text-3xl flex items-center justify-center rounded-full bg-lightGreen text-green  cursor-pointer"
+            href="https://t.me/H_Tomic"
+            target="_blank"
+          >
+            <FaTelegramPlane />
+          </motion.a>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ app/projects/components/projectModal.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import { modalState } from "@/atoms/projectAtom";
+import { AnimatePresence, motion } from "framer-motion";
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
+import { useRecoilState } from "recoil";
+import { projects } from "@/public/projectsContent";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import Image from "next/image";
+import "@splidejs/react-splide/css";
+import { HiExternalLink } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
+function ProjectModal() {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+
+  const project = projects.find((project) => project.id === modalOpen);
+
+  console.log(project);
+
+  return (
+    <AnimatePresence mode="wait">
+    <motion.div
+    key={"47"}
+     initial={{ opacity: 0}}
+     animate={{ opacity: 1 }}
+     transition={{ duration: 0.7, ease: "easeInOut" }}
+     exit={{ opacity: 0 }}
+      className="w-full h-auto left-0 top-0 text-pink flex font-poppins flex-col gap-12 items-center  justify-start md:mb-28 mb-14"
+    >
+      <div className="flex flex-row items-center p-6 w-full justify-between">
+        <h1 className="md:text-4xl text-2xl  font-bold italic">
+          {project?.name}
+        </h1>
+        <motion.button
+        
+          whileHover={{
+            rotate: 360,
+            scale: 1.1,
+            backgroundColor:"#DDACE6",
+
+            transition: { duration: 0.8, type: "spring" },
+          }}
+          whileTap={{ rotate: -20, scale: 0.8 }}
+          onClick={() => setModalOpen(null)}
+          className="md:h-14 md:w-14 h-10 w-10 flex items-center justify-center rounded-full bg-lightGreen text-green md:text-3xl text-xl "
+        >
+          <MdOutlineKeyboardBackspace />
+        </motion.button>
+      </div>
+      <div className="flex flex-col justify-start items-center md:max-w-4xl gap-12 w-full min-h-min xl:max-w-full">
+  
+        <Splide
+          options={{
+            type: "slide",
+            gap:"3rem",
+            arrows: false,
+            pagination: true,
+            paginationDirection: "ttb",
+            rewind: true,
+            autoplay: true,
+            pauseOnHover: true,
+          }}
+          className="flex justify-start items-start  xl:w-2/3  w-11/12 custom rounded-md"
+        >
+          {project?.images.map((image, index) => {
+            return (
+              <SplideSlide
+                data-splide-interval="3000"
+                className="flex justify-center items-center mb-4  lg:h-[600px] h-[400] "
+              >
+                <img
+                  src={image.src}           	
+                  alt={image.alt}
+                  className="object-scale-down w-full h-auto rounded-md"
+                />
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+
+      
+
+        <div className="flex flex-row flex-wrap justify-center text-3xl gap-8 mt-4 md:w-1/3 m-4">
+          {project?.technologies.map((technology, index) => {
+            return (
+            <div>{technology.icon}</div>
+            )
+          })}
+        </div>
+        <div className="flex flex-col justify-start mt-8 items-start font-poppins w-3/4 ">
+          <h1 className="text-pink font-semibold text-2xl mb-3">Project Description</h1>
+          <p className="text-lightGreen text-lg">{project?.description.map((paragraph) => {
+            return (
+              <p className="mb-2">{paragraph}</p>
+            )
+          })}</p>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-10 mb-6">
+        <motion.a
+             
+                whileHover={{
+                  rotate: 360,
+                  scale: 1.1,
+                  backgroundColor:"#DDACE6",
+                  transition: { duration: 0.8, type: "spring" },
+                }}
+                whileTap={{ rotate: -20, scale: 0.8 }}
+                className="md:h-14 md:w-14 h-10 w-10 flex items-center justify-center rounded-full bg-lightGreen text-green md:text-3xl text-xl "
+                href={project?.github}
+                target="_blank"
+              >
+                <FaGithubSquare />
+              </motion.a>
+
+              <motion.a
+               
+                whileHover={{
+                  rotate: 360,
+                  scale: 1.1,
+                  backgroundColor:"#DDACE6",
+                  transition: { duration: 0.8, type: "spring" },
+                }}
+                whileTap={{ rotate: -20, scale: 0.8 }}
+                className="md:h-14 md:w-14 h-10 w-10 flex items-center justify-center rounded-full bg-lightGreen text-green md:text-3xl text-xl "
+                href={project?.link}
+                target="_blank"
+              >
+                <HiExternalLink/>
+              </motion.a>
+        </div>
+      </div>
+    </motion.div>
+    </AnimatePresence>
+  );
+}
+
+export default ProjectModal;
